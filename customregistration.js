cdec8826c7b3a8eb120b970ec65a58c0d817a44a
realdavidturnbull <hello@meteortips.com>
2015-04-28 13:51:28 +1000

    Custom Registration Form

---
+++ customregistration.js
@@ -0,0 +1,27 @@
+if (Meteor.isClient) {
+  Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+  });
+  Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
+    }
+  });
+  Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+  });
+}
